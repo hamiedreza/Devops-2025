cc695e3fc94e4d56151052fd5127b722bdb691bd
hamiedreza Asosheh <hr.asosheh71@gmail.com>
2025-09-21 18:16:09 +0330

    Add script11 to send /etc/passwd to reachable server via SCP

---
+++ scripts/script10.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+while [ -z "$num" ]; do
+    read -p "Please enter your Number " num
+done
+
+sleep 2
+
+read -p "Enter number 1: " num
+max=$num
+min=$num
+
+
+for i in {2..20}
+do
+    read -p "Enter number $i: " num
+
+    if [ "$num" -gt "$max" ]; then
+        max=$num
+    fi
+
+    if [ "$num" -lt "$min" ]; then
+        min=$num
+    fi
+done
+
+echo "Your favorite OS is: $MY_OS"
+echo "The largest number is: $max"
+echo "The smallest number is: $min"
+
